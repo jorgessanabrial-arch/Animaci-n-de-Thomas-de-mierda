d40b3118d5676c862f31b966d3627f89a7ee20d3
SANS <jorgessanabrial@gmail.com>
2026-09-08 11:02:28 -0500

    Add files via upload

---
+++ img/chasis.js
@@ -0,0 +1,14 @@
+﻿(function(window) {
+Símbolo_5_instancia_1 = function() {
+	this.initialize();
+}
+Símbolo_5_instancia_1._SpriteSheet = new createjs.SpriteSheet({images: ["chasis.png"], frames: [[0,0,625,323,0,0,0]]});
+var Símbolo_5_instancia_1_p = Símbolo_5_instancia_1.prototype = new createjs.Sprite();
+Símbolo_5_instancia_1_p.Sprite_initialize = Símbolo_5_instancia_1_p.initialize;
+Símbolo_5_instancia_1_p.initialize = function() {
+	this.Sprite_initialize(Símbolo_5_instancia_1._SpriteSheet);
+	this.paused = false;
+}
+window.Símbolo_5_instancia_1 = Símbolo_5_instancia_1;
+}(window));
+
---
+++ img/luz.js
@@ -0,0 +1,14 @@
+﻿(function(window) {
+mc_luz_instancia_1 = function() {
+	this.initialize();
+}
+mc_luz_instancia_1._SpriteSheet = new createjs.SpriteSheet({images: ["luz.png"], frames: [[0,0,197,172,0,170.1,72.85],[197,0,197,172,0,170.1,72.85]]});
+var mc_luz_instancia_1_p = mc_luz_instancia_1.prototype = new createjs.Sprite();
+mc_luz_instancia_1_p.Sprite_initialize = mc_luz_instancia_1_p.initialize;
+mc_luz_instancia_1_p.initialize = function() {
+	this.Sprite_initialize(mc_luz_instancia_1._SpriteSheet);
+	this.paused = false;
+}
+window.mc_luz_instancia_1 = mc_luz_instancia_1;
+}(window));
+
